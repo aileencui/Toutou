a13bacd383b27aa992f063dfdb550ea1272c4f5e
TowerGit <jianghe239@qq.com>
2016-04-03 00:27:38 +0800

    create project

---
+++ Toutou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Toutou
+//
+//  Created by AlineCui on 16/4/3.
+//  Copyright © 2016年 AlineCui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Toutou/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Toutou
+//
+//  Created by AlineCui on 16/4/3.
+//  Copyright © 2016年 AlineCui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
